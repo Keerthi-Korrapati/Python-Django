85cf34177dd946da4fb7e1d6be39c8033ad8cce1
Keerthi-Korrapati <103488773+Keerthi-Korrapati@users.noreply.github.com>
2023-10-25 12:54:33 +0530

    Adding python practice codes

---
+++ Python-Concepts-Practice/07_booleans/code.py
@@ -0,0 +1,13 @@
+# -- Comparisons --
+
+print(5 == 5)  # True
+print(5 > 5)  # False
+print(10 != 10)  # False
+
+# Comparisons: ==, !=, >, <, >=, <=
+
+friends = ["Rolf", "Bob"]
+abroad = ["Rolf", "Bob"]
+
+print(friends == abroad)  # True
+print(friends is abroad)  # False
